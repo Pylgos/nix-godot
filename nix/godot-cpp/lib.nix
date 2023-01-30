fe94bd0d1e450968c9412889cafcb7d281d03072
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-01-30 22:56:42 +0900

    update

--- nix/godot-cpp/lib.nix
+++ nix/godot-cpp/lib.nix
@@ -7,9 +7,14 @@ in
   buildGodotCpp = godot-headers:
     let in
     nixpkgs.stdenv.mkDerivation {
-      pname = "godot-cpp";
-      version = "idk";
+      name = "godot-cpp";
+
       src = inputs.godot-cpp-source;
+
+      cmakeFlags = [
+        "-DGODOT_GDEXTENSION_DIR=${godot-headers}"
+      ];
+
       nativeBuildInputs = with nixpkgs; [
         cmake
         python3
