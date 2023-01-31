4245878453381792d43dcf6c3ac4afeda6b23099
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-01-31 14:04:57 +0900

    update

--- nix/godot-cpp/packages.nix
+++ nix/godot-cpp/packages.nix
@@ -2,4 +2,11 @@
 let
 in
 {
+  godot-master-test-extension = nixpkgs.stdenv.mkDerivation {
+    name = "test-extension";
+    src = inputs.self + /test;
+    cmakeFlags = [ "-GNinja" ];
+    nativeBuildInputs = [ nixpkgs.cmake nixpkgs.ninja ];
+    buildInputs = [ inputs.cells.godot.packages.godot-master.godot-cpp ];
+  };
 }
--- nix/godot/packages.nix
+++ nix/godot/packages.nix
@@ -7,15 +7,4 @@ let
 in
 {
   godot-master = buildGodot4 { src = inputs.godot-master-source; };
-
-  test-extension = nixpkgs.stdenv.mkDerivation {
-    name = "test-extension";
-    src = inputs.self + /test;
-    cmakeFlags = [ "-GNinja" ];
-    nativeBuildInputs = [ nixpkgs.cmake nixpkgs.ninja ];
-    buildInputs = [ cell.packages.godot-master.godot-cpp ];
-    preConfigure = ''
-      env
-    '';
-  };
 }
