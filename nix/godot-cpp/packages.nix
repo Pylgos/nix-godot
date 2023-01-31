0299148549db7860769fb438f7bf27bf9ca3a0bf
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-01-31 14:06:53 +0900

    fix

--- nix/godot-cpp/packages.nix
+++ nix/godot-cpp/packages.nix
@@ -4,7 +4,7 @@ in
 {
   godot-master-test-extension = nixpkgs.stdenv.mkDerivation {
     name = "test-extension";
-    src = inputs.self + /test;
+    src = inputs.self + /test/godot-cpp-test;
     cmakeFlags = [ "-GNinja" ];
     nativeBuildInputs = [ nixpkgs.cmake nixpkgs.ninja ];
     buildInputs = [ inputs.cells.godot.packages.godot-master.godot-cpp ];
