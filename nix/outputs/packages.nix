17bcdcae21dcdfbdd75ab9f255af04cf70b10029
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-04-06 09:51:37 +0900

    oops

--- nix/outputs/packages.nix
+++ nix/outputs/packages.nix
@@ -3,6 +3,6 @@ let
   inherit (inputs) cells;
 in
 {
-  inherit (cells.godot.packages) godot-master;
+  inherit (cells.godot.packages) godot-master godot-master-debug;
   inherit (cells.godot-cpp.packages) demo-extension;
 }
